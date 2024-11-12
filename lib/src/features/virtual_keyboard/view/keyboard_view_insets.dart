a3ac84706bc1352fbcff63ad8aac247ff29d92c2
AdrianMargineanu <a.margineanu98@gmail.com>
2024-11-12 10:27:34 +0200

    remove comments

--- lib/src/features/virtual_keyboard/view/keyboard_view_insets.dart
+++ lib/src/features/virtual_keyboard/view/keyboard_view_insets.dart
@@ -2,82 +2,25 @@ import 'dart:math';
 
 import 'package:flutter/material.dart';
 
-/// Widget for math keyboards ensuring the content is pushed up by the
-/// keyboards, connected with [MathField]s.
-///
-/// It ensures that the **view insets** for the keyboards are properly
-/// maintained in the [MediaQuery] in scope.
-///
-/// Using this scaffold has two requirements:
-///
-/// * A [WidgetsApp] or [MaterialApp] **above** this widget that inserts a
-///   [MediaQuery]. You can alternatively of course manually maintain the
-///   [MediaQuery] above this widget, but it is not recommended.
-/// * A [Scaffold], [SafeArea], [CupertinoTabScaffold], or
-///   [CupertinoPageScaffold] **below** this widget that respond to the *view
-///   insets* in the [MediaQueryData]. You can alternatively respond to the
-///   view insets yourself using `MediaQuery.of(context).viewInsets`.
-///
-/// In this context, "above" means as a parent of this widget and "below" means
-/// as a child of this widget.
-///
-/// ### Example
-///
-/// ```dart
-/// void main() {
-///   runApp(MaterialApp(
-///     // ...
-///   ));
-/// }
-///
-/// @override
-/// Widget build(BuildContext context) {
-///   return MathKeyboardViewInsets(
-///     child: Scaffold(
-///       // ...
-///     ),
-///   );
-/// }
-/// ```
-///
-/// ### Notes
-///
-/// This widget inserts a [KeyboardViewInsetsQuery] - this is the only
-/// functions at the moment.
 class KeyboardViewInsets extends StatefulWidget {
-  /// Creates a [KeyboardViewInsets] widget around the [child] widget.
   const KeyboardViewInsets({
     super.key,
     required this.child,
   });
 
-  /// The child widget tree that the math keyboard view insets should report to.
-  ///
-  /// Usually a `Scaffold`. Do note, however, that the `Scaffold`, `SafeArea`,
-  /// etc. can be as deep in the tree below the [KeyboardViewInsets] as you
-  /// like.
   final Widget child;
 
   @override
   KeyboardViewInsetsState createState() => KeyboardViewInsetsState();
 }
 
-/// State of the [KeyboardViewInsets].
-///
-/// This takes care of managing the view insets for all child [MathField]
-/// keyboards. It provides them by inserting a modified [MediaQuery].
 class KeyboardViewInsetsState extends State<KeyboardViewInsets> {
-  /// Returns the ancestor [KeyboardViewInsetsState] of the given [context].
   static KeyboardViewInsetsState? of(BuildContext context) {
     return context.findAncestorStateOfType<KeyboardViewInsetsState>();
   }
 
   final Map<ObjectKey, double> _keyboardSizes = {};
 
-  /// Reports the [size] of the math keyboard (state) with the given [key].
-  ///
-  /// Pass `null` for [size] to report that a keyboard has been removed from
-  /// the scaffold. This is important for preventing memory leaks.
   void operator []=(ObjectKey key, double? size) {
     if (!mounted) return;
     if (_keyboardSizes[key] == size) return;
@@ -91,10 +34,6 @@ class KeyboardViewInsetsState extends State<KeyboardViewInsets> {
     });
   }
 
-  /// The appropriate bottom inset based on the [_keyboardSizes].
-  ///
-  /// This will simply take the max value from the [_keyboardSizes] and default
-  /// to `0` (when there are no active keyboards).
   double get _bottomInset {
     return _keyboardSizes.values.fold(0, (previousValue, element) {
       // We only care about the max inset.
@@ -131,14 +70,7 @@ class KeyboardViewInsetsState extends State<KeyboardViewInsets> {
   }
 }
 
-/// Inherited widget, which is purely responsible for notifying about the passed
-/// [bottomInset].
-///
-/// This allows checking whether a math keyboard (or any keyboard really, for
-/// convenience) is currently showing. See [].
 class KeyboardViewInsetsQuery extends InheritedWidget {
-  /// Creates a [KeyboardViewInsetsQuery] that provides the [bottomInset] to
-  /// the [child] tree.
   const KeyboardViewInsetsQuery({
     super.key,
     required this.bottomInset,
@@ -157,58 +89,10 @@ class KeyboardViewInsetsQuery extends InheritedWidget {
         'contain a MathKeyboardViewInsetsQuery.');
   }
 
-  /// Returns whether any math keyboard is showing in the given [context] by
-  /// depending on a [KeyboardViewInsetsQuery].
-  ///
-  /// See [keyboardShowingIn] for a convenience method that also reports about
-  /// the regular software keyboard on iOS and Android.
   static bool virtualKeyboardShowingIn(BuildContext context) {
     return of(context).bottomInset > 0;
   }
 
-  /// Returns whether any software keyboard is showing in the given [context]
-  /// by depending on a [KeyboardViewInsetsQuery] and consulting the
-  /// [WidgetsBinding] window.
-  ///
-  /// This is useful to you when you want to take an action whenever *any*
-  /// keyboard is currently opened up. The math keyboard package and sadly
-  /// not change the view insets on the [WidgetsBinding] instance's [Window],
-  /// which means that you need to use this helper for checking if a math
-  /// keyboard is currently opened up.
-  ///
-  /// Note that we cannot consult the [MediaQuery] for regular software
-  /// keyboards as widgets like [Scaffold] consume the bottom inset. We can,
-  /// however, safely depend on [KeyboardViewInsetsQuery] as we know that
-  /// the bottom inset will not be consumed going down the tree.
-  ///
-  /// See [virtualKeyboardShowingIn] for a method that reports only whether a
-  /// math keyboard is showing, ignoring other software keyboards.
-  ///
-  /// ### Example
-  ///
-  /// ```dart
-  /// @override
-  /// Widget build(BuildContext context) {
-  ///   final isAnyKeyboardShowing =
-  ///       MathKeyboardViewInsetsQuery.keyboardShowingIn(context);
-  /// }
-  /// ```
-  ///
-  /// `isAnyKeyboardShowing` will now tell you whether there *any* software
-  /// keyboard is showing, i.e. the default software keyboard on Android and
-  /// iOS *or* a math keyboard.
-  ///
-  /// Of course, you need to make sure that there is a [KeyboardViewInsets]
-  /// widget as a parent of the `content`.
-  ///
-  /// By default, this will only notify you about changes to the
-  /// [KeyboardViewInsetsQuery] and not about changes to the other software
-  /// keyboards. If you want to be notified about changes to the other software
-  /// keyboards also , you will have to register a [WidgetsBindingObserver] and
-  /// set state on [WidgetsBindingObserver.didChangeMetrics].
-  /// See [the official `WidgetsBindingObserver` example](https://api.flutter.dev/flutter/widgets/WidgetsBindingObserver-class.html#widgets.WidgetsBindingObserver.1)
-  /// and make sure to set state on `didChangeMetrics` instead of
-  /// `didChangeAppLifecycleState` :)
   static bool keyboardShowingIn(BuildContext context) {
     final maxInset = max(
       of(context).bottomInset,
@@ -222,13 +106,6 @@ class KeyboardViewInsetsQuery extends InheritedWidget {
     return maxInset > 0;
   }
 
-  /// The inset at the bottom of the screen accounting *only* for math
-  /// keyboards.
-  ///
-  /// This does not yet take the [Window] into account.
-  ///
-  /// The [KeyboardViewInsets] widget will insert a [MediaQuery] that also
-  /// takes into account the window below this widget.
   final double bottomInset;
 
   @override
